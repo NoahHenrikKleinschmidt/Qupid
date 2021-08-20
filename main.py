e81a5b4e62184412e3f885d25ec697ddb6e9b0ab
NoahHenrikKleinschmidt <89252165+NoahHenrikKleinschmidt@users.noreply.github.com>
2021-08-21 01:04:52 +0200

    Update main.py

--- main.py
+++ main.py
@@ -145,7 +145,9 @@ def main():
         
     except Exception as e: pass #st.write(e)
 
-   
+    col2.markdown("""
+    ---
+    """)
     citation = """Kleinschmidt, N. (2021). qpcr-Analyser -- a web-based application to facilitate qPCR data analysis (Version 0.0.1) [Computer software]. https://github.com/NoahHenrikKleinschmidt/qpcr-Analyser.git"""
     col2.markdown("""
     When using this app to analyse your data, please cite: \n\n {}

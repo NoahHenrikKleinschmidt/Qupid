d0883123e727be695c167b92cb352abeff854b92
NoahHenrikKleinschmidt <89252165+NoahHenrikKleinschmidt@users.noreply.github.com>
2021-08-20 19:05:50 +0200

    Add files via upload

---
+++ qPCR App/data_auxiliary.py
@@ -0,0 +1,199 @@
+from os import remove
+import pandas as pd
+import  qpcr
+import qpcr.Analysis as qA 
+from copy import deepcopy
+import base64
+import streamlit as st
+import zipfile
+import io
+import matplotlib.pyplot as plt
+import statistics as stat
+
+# all of the import data, read data and open file work together, let's not question why...
+def import_data(data_obj):
+    data = read_data(data_obj)
+    if isinstance(data, list):
+        overall = []
+        for i in data:
+            overall.append(open_file(i))
+        return overall
+    else:
+        data = open_file(data)
+        return data
+
+def read_data(data_obj):
+    if isinstance(data_obj, list):
+        overall = []
+        for d in data_obj:
+            obj1 = deepcopy(d)
+            tmp1 = pd.read_csv(d, delimiter=";")
+            tmp2 = pd.read_csv(obj1)
+            cols1 = [i for i in tmp1.columns]
+            cols2 = [i for i in tmp2.columns]
+            if len(cols1) > len(cols2):
+                overall.append(tmp1)
+            else: 
+                overall.append(tmp2)
+        return overall
+    else: 
+        obj1 = deepcopy(data_obj) #this deepcopy nonsense is actually recuired as the data_obj appears to get "used up" once read_csv has been performed...
+        tmp1 = pd.read_csv(data_obj, delimiter=";")
+        tmp2 = pd.read_csv(obj1)
+        cols1 = [i for i in tmp1.columns]
+        cols2 = [i for i in tmp2.columns]
+        if len(cols1) > len(cols2):
+            return tmp1
+        else:
+            return tmp2
+
+
+
+def open_file(dataframe, export='dict'):
+    contents = convert_to_lines(dataframe)
+
+    if ";" in contents[0]:
+        contents = [i.replace(';', ',') for i in contents]
+    contents = [i.replace('\n', '') for i in contents]
+
+    #now split the raw lines into lists each
+    contents = [i.split(',') for i in contents]
+    
+    #now remove the unnecessary lines
+    temp = []
+    for i in contents:
+        if i[1] != '':
+            temp.append(i)
+    contents = temp
+    if export == 'list':
+        return contents
+    elif export == 'dict':
+        new_dict = {
+            'Sample' : [i[0] for i in contents[0:]],
+            'Mean Ct' : [i[1] for i in contents[0:]],
+            #'Stdev' : [i[2] for i in contents[1:]],
+
+        }
+        return new_dict
+
+def convert_to_lines(dataframe):
+    idx = 0
+    lines = []
+    while True:
+        try: 
+            string = ""
+            for i in dataframe.keys():
+                string = "{},{}".format(string, dataframe[i][idx])
+            lines.append(string[1:])    
+            idx += 1
+        except: 
+            break
+    return lines
+
+# process the group names, if provided...
+
+def get_group_names(group_name_field):
+    if group_name_field == "":
+        group_names = None
+    else: 
+        group_names = group_name_field.split(",")
+        group_names = [i.strip() for i in group_names]
+    return group_names
+
+# re-convert data-dict from results into displayable dataframe
+
+def dict_to_frame(data_dict):
+    frame = pd.DataFrame(data_dict)
+    return frame
+
+# generate download link for the results csv file
+def generate_download_link(results_dict, filename, analysis):
+    result_frame = pd.DataFrame(results_dict)
+    csv_file = result_frame.to_csv(index=False)
+    b64 = base64.b64encode(csv_file.encode()).decode()
+    filename = "{}_{}.csv".format(filename, analysis[0])
+    href = f'<a href="data:file/csv;base64,{b64}" download="{filename}">Download Results as CSV</a>'
+    return href
+
+# generate a zip download link
+
+def generate_zip_download_link(zip):
+    filename = "results.zip"
+    b64 = base64.b64encode(zip.encode()).decode()
+    href = f'<a href="data:file/zip;base64,{b64}" download="{filename}">Download Results as ZIP</a>'
+    return href
+
+# generate run_names
+def generate_run_names(target_files, normaliser):
+    norm_name = remove_suffix(normaliser)
+    target_names = [remove_suffix(i) for i in target_files]
+    run_names = ["{}_rel_{}_ddct".format(i, norm_name) for i in target_names]
+    return run_names
+
+def remove_suffix(file):
+    norm_name = file.name
+    norm_name = norm_name.split(".")
+    norm_name = norm_name[0]
+    return norm_name
+
+
+    
+# combine normalisers functions (these will take the place of the qpcr functions)
+
+# not yet edited! 
+
+
+#if several normalisers should be used they first have to be pre-processed to get their Delta_Ct values before they can be combined using combine_normalisers
+def preprocess_normalisers(normalisers:list, replicates, run_names, group_names=None, anchor=None, return_type='list'):
+    normalisers_dict = {}
+    ndx = 0
+    for norm in normalisers:
+        try: 
+            name = run_names[ndx]
+        
+            tmp = norm
+            tmp = qpcr.group_samples(tmp, replicates=replicates)
+            if group_names is not None:
+                tmp = qpcr.rename_groups(tmp, new_names=group_names)
+            tmp = qpcr.Delta_Ct(tmp, anchor=anchor)
+            tmp = {name : tmp}
+            normalisers_dict.update(tmp)
+            ndx +=1
+            if return_type == 'list':
+                temp = []
+                for i in run_names:
+                    temp.append(normalisers_dict[i])
+                normalisers_dict = list(temp)
+        except: pass    
+    return normalisers_dict
+
+#if several normalisers should be averaged, use combine_normalisers
+def combine_normalisers(normalisers:list):
+    combined_normaliser = {}
+    keys = list(normalisers[0].keys()) #get all groupings
+    for k in keys:
+        temp = []
+        for norm in normalisers:
+            length = len(norm[k])
+            assert isinstance(norm[k][0], float), "norm[k][0] is not a simple number but: {} ... \nAre you sure you already performed qpcr.Delta_Ct on your normaliser?".format(norm[k][0])
+            for i in norm[k]:
+                temp.append(i)
+        temp = {k : [stat.mean(temp) for i in range(length)]} #we must conserve dimensionality...
+        combined_normaliser.update(temp)
+    return combined_normaliser
+
+# generate run_names
+def generate_normaliser_combined_names(normaliser_file, target_file):
+    names = [remove_suffix(normaliser_file)]
+    for t in target_file:
+        names.append(remove_suffix(t))
+    return names
+
+if __name__ == '__main__':
+    n1 = "/Users/NoahHK/OneDrive - Universitaet Bern/Bachelor Project/qPCR/Week 5/16.03.21/Pre-Processed/28S.csv"
+    n2 = "/Users/NoahHK/OneDrive - Universitaet Bern/Bachelor Project/qPCR/Week 5/16.03.21/Pre-Processed/HNRNPL_nmd.csv"
+    n3 = "/Users/NoahHK/OneDrive - Universitaet Bern/Bachelor Project/qPCR/Week 5/16.03.21/Pre-Processed/HNRNPL_prot.csv"
+
+    prepped = qpcr.preprocess_normalisers([n1, n2, n3], 6, ["28S", "TRA_NMD", "HNprot"], anchor="first")
+    normed = qpcr.combine_normalisers(prepped)
+    print(normed)
---
+++ qPCR App/main.py
@@ -0,0 +1,193 @@
+import streamlit as st
+import pandas as pd
+import qpcr
+from copy import deepcopy
+from pathlib import Path
+
+from data_auxiliary import * 
+from data_analysis import *
+
+def main():
+    st.title("qPCR Analysis Tool")
+
+    # get the input data files
+    expander = st.expander("Upload your files here")
+    e_col1, e_col2 = expander.columns(2)
+    normaliser_file = e_col1.file_uploader("Select csv file for the normaliser", type=["csv"])
+    target_file = e_col2.file_uploader("Select csv file(s) for your target runs", type=["csv"], accept_multiple_files=True)
+
+    if target_file:
+        target = import_data(target_file)
+    if normaliser_file:
+        norm_backup = deepcopy(normaliser_file)
+        normaliser = import_data(normaliser_file)
+
+    # setup the settings containers
+    container = st.container()
+    col1, col2 = container.columns(2)
+
+    replicate_type = col1.selectbox("Select what type of replicates to use", ["equal replicates (n)", "specified (list)"])
+
+    if replicate_type == "equal replicates (n)":
+        replicates = col1.number_input("Define the number of replicates",
+                            min_value = 1, step = 1)
+    elif replicate_type == "specified (list)":
+        replicates = col1.text_input("Provide a list of replicates (n) for each group")
+        replicates = replicates.split(",")
+        try: 
+            replicates = [int(i) for i in replicates]
+        except: pass
+        
+    anchor = col1.selectbox("Select what anchor to use", 
+                    ["grouped", "first", "specified"])
+    
+    if anchor == "specified":
+        new_anchor = col1.number_input("Specify the anchor value")
+        if new_anchor:
+            anchor = new_anchor
+    elif anchor == "grouped":
+        anchor = None
+
+    groupnames = col1.text_input("Insert group names here (optional)")
+    groupnames = get_group_names(groupnames)
+
+    mode = col1.selectbox("Mode of analysis", ["replicate", "stats"])
+
+    use_combined_normalisers = col1.checkbox("Uses combined normalisers")
+
+    # setup the analysis controls in the second column 
+
+    col2.markdown("## Available Analysis")
+
+    # single delta ct analysis – basically normalisation within one data-file
+    single_delta_ct = col2.button("Single Delta CT")
+    if single_delta_ct:
+        try: 
+            var = target
+        except: 
+            st.error("At least one target file is required!")
+        
+        analysis = ["Single Delta CT", "$\Delta$CT"] #just as a label for the plot later on...
+
+        if len(target) > 1:
+            st.error("Single Delta CT only works on one target file!")
+        else:
+            result = single_dCt(target[0], replicates, mode, anchor=anchor, group_names = groupnames, export=False)
+            result_filename = target_file[0].name
+    
+    # delta delta CT analysis 
+    delta_delta_ct = col2.button("Delta Delta CT")
+    if delta_delta_ct:
+        try: 
+            var = normaliser
+            var = target
+        except:
+            st.error("Make sure you have both one normaliser and at least one target file selected!")
+
+        analysis = ["Delta Delta CT", "$\Delta\Delta$CT"]
+
+        data_files = []
+        for i in target:
+            data_files.append(deepcopy(i))
+        
+        # run names are automatically assigned from the file-names
+        run_names = generate_run_names(target_file, normaliser_file)
+
+        # when using combined normalisers we need to specially import the file so as to avoid the pre-processing 
+        if use_combined_normalisers == True:
+            norm = read_data(norm_backup)
+            norm = norm.to_dict()
+            normaliser = norm
+        
+        result = delta_dCt(data_files, replicates, deepcopy(normaliser), run_names, group_names=groupnames, anchor=anchor, export=False, use_combined_normalisers=use_combined_normalisers)
+
+    # combine normalisers
+    combine_normaliser = col2.button("Combine Normalisers")
+    if combine_normaliser:
+        try: 
+            var = normaliser
+            var = target
+        except: 
+            st.error("Please, select one normaliser and at least one target which shall be combined with the normalisers")
+        
+        to_combine = [normaliser]
+        for i in target:
+            to_combine.append(i)
+        
+        run_names = generate_normaliser_combined_names(normaliser_file,target_file)
+
+        to_combine = preprocess_normalisers(to_combine, replicates, run_names, groupnames, anchor=anchor)
+        result = combine_normalisers(to_combine)
+        
+        analysis = ["combine", ""]
+
+    container2 = st.container()
+
+    try: 
+        if analysis[0] == "Single Delta CT":
+            display_results_singleCT(container2, mode, analysis, result, result_filename)
+        elif analysis[0] == "Delta Delta CT":
+            container2.markdown("Your results have been compiled into a ZIP file and placed in your Downloads folder")
+            print_figs = display_results_ddCT(container2, mode, analysis, result)            
+            zip_result = convert_to_stats(mode, result)
+            zip_compiler(zip_result, print_figs)
+        elif analysis[0] == "combine":
+            container.write(dict_to_frame(result), use_container_width=True)
+            download_link = generate_download_link(result, "-".join(run_names), analysis)
+            container.markdown(download_link, unsafe_allow_html=True)
+    except Exception as e: pass #st.write(e)
+
+def convert_to_stats(mode, result):
+    if mode == "stats":
+        zip_result = {}
+        for i in result:
+            tmp = {i : qpcr.get_stats(result[i])}
+            zip_result.update(tmp)
+    else:
+        zip_result = result
+    return zip_result
+
+def display_results_singleCT(container, mode, analysis, result, result_filename):
+    if result:
+        barplot = barchart(result, mode, analysis, result_filename)
+        container.plotly_chart(barplot, use_container_width=True)
+        container.write(dict_to_frame(result), use_container_width=True)
+        download_link = generate_download_link(result, result_filename, analysis)
+        container.markdown(download_link, unsafe_allow_html=True)
+
+def display_results_ddCT(container, mode, analysis, result):
+    if result:
+        print_figs = []
+        for d in result:
+            fig = barchart(result[d], mode, analysis, d)
+            container.plotly_chart(fig, use_container_width=True)
+            f1 = print_chart(result[d], mode, analysis, d)
+            print_figs.append(f1)
+    return print_figs
+
+
+
+
+def zip_compiler(result, print_figs):
+    filename = "{}/Downloads/results.zip".format(Path.home())
+    with zipfile.ZipFile(filename, mode="w") as zf:
+                # store figures
+        for f in print_figs:
+            name = "{}.jpg".format(f.axes[0].get_title())
+            buf = io.BytesIO()
+            f.savefig(buf, dpi=150)
+            plt.close()
+            zf.writestr(name, data=buf.getbuffer())
+                
+                # store the dict entries as csv files
+        for d in result:
+            buf = io.StringIO()
+            csv = result[d]
+            csv = pd.DataFrame(csv)
+            csv = csv.to_csv(buf)
+            name = "{}.csv".format(d)
+            zf.writestr(name, data=buf.getvalue())
+
+
+if __name__=="__main__":
+    main()

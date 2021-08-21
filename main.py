ad03091381f135c3e242d5e21e89a2f5323a569e
NoahHenrikKleinschmidt <89252165+NoahHenrikKleinschmidt@users.noreply.github.com>
2021-08-21 14:36:27 +0200

    Update main.py

--- main.py
+++ main.py
@@ -217,7 +217,7 @@ def zip_compiler(result, print_figs):
         # now download link
         st.write("zf:")
         st.write(zf)
-        b64 = base64.b64encode(zf).decode()
+        b64 = base64.b64encode(zf.encode()).decode()
         st.write("b64:")
         st.write(b64)
         href = f'<a href="data:file/zip;base64,{b64}" download=\'{filename}\'>\

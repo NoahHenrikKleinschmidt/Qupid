761f5c3b29a0da10d7a87a0cc77475d74eba7cb4
NoahHenrikKleinschmidt <89252165+NoahHenrikKleinschmidt@users.noreply.github.com>
2021-08-22 19:12:08 +0200

    Update main.py

--- main.py
+++ main.py
@@ -143,9 +143,9 @@ def main():
             fig = qA.make_aggregate_plot(result)
             agg_exp.pyplot(fig)
             grp_exp = container2.expander("Expand to view grouped plots")
-            fig = qA.make_grouped_plots(result)
+            fig = qA.make_grouped_plots(result, transpose=True)
             grp_exp.pyplot(fig)
-            
+
             print_figs = display_results_ddCT(container2, mode, analysis, result)            
             zip_result = convert_to_stats(mode, result)
             zip_compiler(col1, zip_result, print_figs)

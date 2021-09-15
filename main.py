df431728e4279665da51e65e35241de7283a6327
NoahHenrikKleinschmidt <89252165+NoahHenrikKleinschmidt@users.noreply.github.com>
2021-09-15 18:51:59 +0200

    Update main.py
    Changed the make_grouped_plots specs to first_only and cutoff 0.26

--- main.py
+++ main.py
@@ -147,7 +147,7 @@ def main():
             agg_exp.pyplot(fig)
             grp_exp = container2.expander("Expand to view grouped plots")
             try: 
-                fig1 = qA.make_grouped_plots(result, subplots=True, first_only_delimiter="_rel_")
+                fig1 = qA.make_grouped_plots(result, subplots=True,first_only_delimiter="_rel_", match_first_only=True, cutoff=0.27)
                 grp_exp.pyplot(fig1)
                 link = generate_dict_download_link(result)
                 grp_exp.markdown("Not quite happy with this figure? \n {} and open them in [Figed](https://share.streamlit.io/noahhenrikkleinschmidt/figed-for-grouped_plots/main/main.py)  – our app to modify the grouped_plots figure.".format(link), unsafe_allow_html=True)
